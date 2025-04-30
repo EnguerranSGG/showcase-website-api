160e494f399bfd593811abe7c74522ea84e59010
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-04-30 16:15:44 +0200

    feat: add Swagger documentation support and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,23 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Showcase API')
+    .setDescription('Documentation de l’API du site vitrine')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000, '0.0.0.0');
 }
 
