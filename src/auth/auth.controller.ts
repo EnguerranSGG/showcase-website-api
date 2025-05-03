187ba0ba1a6501394c23fda4606f15ef52f91c67
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-04 00:15:51 +0200

    feat: implement signup and login methods in AuthService and corresponding endpoints in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  @ApiOperation({ summary: 'Créer un nouvel utilisateur' })
+  @ApiResponse({ status: 201, description: 'Utilisateur créé avec succès.' })
+  @ApiResponse({ status: 400, description: 'Requête invalide.' })
+  signup(@Body() dto: RegisterDto) {
+    return this.authService.signup(dto);
+  }
+
+  @Post('login')
+  @ApiOperation({ summary: 'Se connecter' })
+  @ApiResponse({ status: 200, description: 'Connexion réussie. Retourne un JWT.' })
+  @ApiResponse({ status: 401, description: 'Identifiants invalides.' })
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService
+    ) {}
+
+  async signup(dto: RegisterDto) {
+    const userExists = await this.prisma.user.findUnique({
+      where: { mail: dto.mail },
+    });
+
+    if (userExists) {
+      throw new ConflictException('Email déjà utilisé');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        firstname: dto.firstname,
+        lastname: dto.lastname,
+        mail: dto.mail,
+        password: hashedPassword,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    });
+
+    return {
+      message: 'Utilisateur inscrit avec succès',
+      user: {
+        id: user.user_id,
+        mail: user.mail,
+      },
+    };
+  }
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { mail: dto.mail },
+    });
+  
+    if (!user) {
+      throw new UnauthorizedException('Identifiants invalides');
+    }
+  
+    const passwordValid = await bcrypt.compare(dto.password, user.password);
+    if (!passwordValid) {
+      throw new UnauthorizedException('Identifiants invalides');
+    }
+  
+    const payload = {
+      sub: user.user_id,
+      mail: user.mail,
+    };
+  
+    const token = await this.jwtService.signAsync(payload);
+  
+    return {
+      access_token: token,
+    };
+  }
+}
