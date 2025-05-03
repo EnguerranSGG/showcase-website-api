d3c6e32430e68f051ad2c8b4dba1b3647663f162
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-03 15:40:23 +0200

    feat: refactor Auth module to use async JWT configuration and add JwtConfig class

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
-import { PrismaService } from '../prisma/prisma.service'; // adapter selon ton chemin
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtConfig } from './jwt.config'; 
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'supersecret', // à sécuriser dans un .env
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      useClass: JwtConfig,
     }),
   ],
+  providers: [AuthService, JwtStrategy, PrismaService, JwtConfig],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService, JwtStrategy],
-  exports: [AuthService],
 })
 export class AuthModule {}
+
