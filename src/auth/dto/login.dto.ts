88c572abd1cde30cef230e15bee66190a42c915e
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-04 00:15:36 +0200

    feat: add ApiProperty decorators to LoginDto and RegisterDto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,10 +1,14 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginDto {
   @IsEmail()
+  @ApiProperty({ example: 'K2m6t@example.com' })
   mail: string;
 
   @IsString()
   @MinLength(6)
+  @ApiProperty({ example: 'password' })
   password: string;
 }
