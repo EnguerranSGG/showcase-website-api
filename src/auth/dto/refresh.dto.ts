12a96c820033f12864a470e035cb51e6bf26d423
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-06 19:14:38 +0200

    feat: add UpdateUserRefreshTokenDto for handling refresh token validation in authentication

---
+++ src/auth/dto/refresh.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
+
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateUserRefreshTokenDto {
+  
+  @IsNotEmpty()
+  @ApiProperty({ example: '123456789' })
+  refreshToken: string;
+}
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -18,4 +18,5 @@ export class RegisterDto {
   @IsNotEmpty()
   @ApiProperty({ example: 'Doe' })
   lastname: string;
+
 }
