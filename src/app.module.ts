0b8f50572a8955d2ee2e2a1578eeb4893eeb28be
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-12 17:47:53 +0200

    feat: add multiple modules (Paths, Steps, JobOffers, Times, Partners, Structures, News, Values) to AppModule for enhanced functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,22 +13,49 @@ import { TypesModule } from './types/types.module';
 import { StatisticsModule } from './statistics/statistics.module';
 import { PositionsModule } from './positions/positions.module';
 import { JobsModule } from './jobs/jobs.module';
+import { PathsModule } from './paths/paths.module';
+import { StepsModule } from './steps/steps.module';
+import { JobOffersModule } from './job-offers/job-offers.module';
+import { TimesModule } from './times/times.module';
+import { PartnersModule } from './partners/partners.module';
+import { StructuresModule } from './structures/structures.module';
+import { NewsModule } from './news/news.module';
+import { ValuesModule } from './values/values.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, TypesModule, StatisticsModule, PositionsModule, JobsModule, LoggerModule,
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    TypesModule,
+    StatisticsModule,
+    PositionsModule,
+    JobsModule,
+    PathsModule,
+    StepsModule,
+    LoggerModule,
+    JobOffersModule,
+    TimesModule,
+    PartnersModule,
+    StructuresModule,
+    NewsModule,
+    ValuesModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV || 'development'}` 
-    })
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_GUARD,
-    useClass: JwtAuthGuard,
-  },
-  {
-    provide: APP_GUARD,
-    useClass: RolesGuard,
-  }],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
