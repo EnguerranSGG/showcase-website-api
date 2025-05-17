ca681bbc5422c12a5379df7dfaba50f3366dd87c
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-17 19:55:32 +0200

    feat: enhance CreateTimeDto with validation rules for year and event description

--- src/times/dto/create-time.dto.ts
+++ src/times/dto/create-time.dto.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateTimeDto {
   @ApiProperty({ example: 2023 })
   @IsInt()
+  @IsNotEmpty()
   year: number;
 
   @ApiProperty({ example: 'Lancement de la plateforme numérique AIR' })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(175)
   event_description: string;
 }
