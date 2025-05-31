372f689a461bf73cedc1b0d6bd5569dc70d37a86
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-31 20:05:28 +0200

    refactor: remove Positions module, including controller, service, and DTOs to streamline codebase and eliminate unused functionality

--- src/positions/dto/create-position.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreatePositionDto {
-  @IsString()
-  @ApiProperty({ example: 'Conseil d\'administration' })
-  name: string;
-}
