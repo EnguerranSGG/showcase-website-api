593dd4701c114a23fff8770474e3e7a1c1f41c72
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-31 19:22:41 +0200

    feat: enhance CreateTimeDto with sanitization for event_description field

--- src/times/dto/create-time.dto.ts
+++ src/times/dto/create-time.dto.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { sanitize } from 'class-sanitizer';
 
 export class CreateTimeDto {
   @ApiProperty({ example: 2023 })
@@ -11,5 +13,6 @@ export class CreateTimeDto {
   @IsString()
   @IsNotEmpty()
   @MaxLength(175)
+  @Transform(({ value }) => sanitize(value))
   event_description: string;
 }
