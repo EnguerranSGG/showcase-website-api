d86675f3aed0089a612a0a6749e462f81de17997
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-31 19:19:41 +0200

    feat: enhance CreateJobOfferDto with validation and sanitization for new fields

--- src/job-offers/dto/create-job-offer.dto.ts
+++ src/job-offers/dto/create-job-offer.dto.ts
@@ -1,35 +1,47 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MaxLength, IsUrl } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { sanitize } from 'class-sanitizer';
 
 export class CreateJobOfferDto {
   @ApiProperty({ example: 'Chargé de mission numérique' })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(60)
+  @Transform(({ value }) => sanitize(value))
   name: string;
 
   @ApiProperty({ example: 'CDD' })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(60)
+  @Transform(({ value }) => sanitize(value))
   job_type: string;
 
   @ApiProperty({ example: 'Lille', required: false })
   @IsString()
   @IsOptional()
+  @MaxLength(50)
+  @Transform(({ value }) => sanitize(value))
   city?: string;
 
   @ApiProperty({ example: 'https://imgur.com/logo.jpg', required: false })
   @IsString()
   @IsOptional()
-  image_url?: string;
-
-  @ApiProperty({ example: 'https://example.com/offre/123', required: false })
-  @IsString()
-  @IsOptional()
+  @MaxLength(255)
+  @IsUrl()
+  @Transform(({ value }) => sanitize(value))
   link?: string;
+
+  @IsOptional()
+  @ApiProperty({ example: 1 })
+  file_id?: number;
 
   @ApiProperty({ example: 'Un poste stimulant dans une structure dynamique' })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(350)
+  @Transform(({ value }) => sanitize(value))
   description: string;
 }
 
