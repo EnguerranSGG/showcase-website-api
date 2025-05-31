735c1a5e31940cb158dbc4ead50497c55096c080
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-05-31 19:26:26 +0200

    feat: enhance CreateNewsDto with additional validation and sanitization, including max length constraints for fields

--- src/news/dto/create-news.dto.ts
+++ src/news/dto/create-news.dto.ts
@@ -1,24 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { sanitize } from 'class-sanitizer';
 
 export class CreateNewsDto {
   @ApiProperty({ example: 'Lancement du nouveau programme' })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(100)
+  @Transform(({ value }) => sanitize(value))
   name: string;
 
-  @ApiProperty({ example: 'https://example.com/image.jpg', required: false })
   @IsOptional()
-  @IsUrl()
-  image_url?: string;
+  file_id?: number;
 
   @ApiProperty({ example: 'https://example.com/article', required: false })
   @IsOptional()
   @IsUrl()
+  @MaxLength(255)
+  @Transform(({ value }) => sanitize(value))
   link?: string;
 
   @ApiProperty({ example: 'Description détaillée de la nouvelle importante.' })
   @IsString()
   @IsNotEmpty()
+  @MaxLength(300)
+  @Transform(({ value }) => sanitize(value))
   description: string;
 }
