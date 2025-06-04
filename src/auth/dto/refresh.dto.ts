438ad409dbc8687d45b60908cf91fe5a9489a623
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-06-04 12:01:11 +0200

    refactor: improve code formatting by removing unnecessary whitespace in refresh.dto.ts and refreshToken.strategy.ts

--- src/auth/dto/refresh.dto.ts
+++ src/auth/dto/refresh.dto.ts
@@ -3,7 +3,6 @@ import { IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserRefreshTokenDto {
-  
   @IsNotEmpty()
   @ApiProperty({ example: '123456789' })
   refreshToken: string;
