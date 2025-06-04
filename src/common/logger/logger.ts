8faa2fba8c35193410a676db9d61c0f25a5be2d7
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-06-04 12:02:30 +0200

    refactor: improve code formatting and readability in logger.ts by restructuring conditional statements and ensuring consistent indentation

--- src/common/logger/logger.ts
+++ src/common/logger/logger.ts
@@ -3,16 +3,17 @@ import pino from 'pino';
 const isProd = process.env.NODE_ENV === 'production';
 const isTTY = process.stdout.isTTY;
 
-export const logger = isProd || !isTTY
-  ? pino({ level: process.env.LOG_LEVEL || 'info' })
-  : pino({
-      level: process.env.LOG_LEVEL || 'debug',
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'yyyy-mm-dd HH:MM:ss.l',
-          ignore: 'pid,hostname',
+export const logger =
+  isProd || !isTTY
+    ? pino({ level: process.env.LOG_LEVEL || 'info' })
+    : pino({
+        level: process.env.LOG_LEVEL || 'debug',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'yyyy-mm-dd HH:MM:ss.l',
+            ignore: 'pid,hostname',
+          },
         },
-      },
-    });
+      });
