047387cbe2ced3ff854a2cd829ff3de4093b2c87
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2025-07-29 11:01:25 +0200

    feat(prisma): update seed data with new presentation texts

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -97,19 +97,14 @@ async function main() {
 
   console.log('Fichiers insérés avec succès.');
 
-  // Insertion de quelques présentations fictives
   const presentations = [
     {
-      presentation_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis nisi lectus, quis egestas est convallis vitae. Donec maximus lobortis massa, vitae ultrices diam aliquet et. Phasellus at porttitor risus, eget elementum dui. Vestibulum molestie congue pretium. Suspendisse consequat nisl at dapibus viverra. Vestibulum est sapien, rhoncus et diam et, porta faucibus risus. Fusce efficitur lectus. ',
+      presentation_text: 'L\'association compte deux centres d’hébergement (CADA, HUDA) à destination des demandeurs d’asile, un centre d\’hébergement (CPH) à destination des bénéficiaires de la protection internationale, un centre d\’hébergement pour femmes isolées, un pôle FLE et une boutique solidaire. L\’historique de l\’association montre la volonté de la direction et du conseil d\’administration d\’innover et de soutenir une dynamique de renouvellement permanent, afin de répondre aux besoins des personnes étrangères, demandeurs d\’asile, réfugiés et de toutes personnes en difficulté.',
       user_id: admin.user_id, //1
     },
     {
-      presentation_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis nisi lectus, quis egestas est convallis vitae. Donec maximus lobortis massa, vitae ultrices diam aliquet et. Phasellus at porttitor risus, eget elementum dui. Vestibulum molestie congue pretium. Suspendisse consequat nisl at dapibus viverra. Vestibulum est sapien, rhoncus et diam et, porta faucibus risus. Fusce efficitur lectus. ',
+      presentation_text: 'L\'association est née de l\'initiative de militants engagés dans le tourisme associatif et social, dans le formation et l\'éducation populaire et d\'une interpellation du secrétariat d\'État chargé de l\'action humanitaire. Le but était de créer un centre d\'accueil, appelé "Centre provisoire d\'hébergement (CPH) dans le Nord-Pas-de-Calais. \nL\'État français avait décidé d\'apporter son aide à la Grande-Bretagne, en accueillant 4400 réfugiés vietnamiens et cambodgiens, présents dans les camps de Hong-Kong (l\'île passant sous la gouvernance chinoise). \nL\'association, régie par la loi du 1er juillet 1901, est donc créée le 5 mars 1990. Le sens voulu de l\'action était d\'accueillir des personnes réfugiées en famille ou isolées et leur permettre une réelle insertion sociale et professionnelle. C\'est ainsi que le premier Centre Provisoire d\'Hébergement (CPH) ouvre ses portes en septembre 1990. \nPour les militants associatifs, il était primordial de permettre aux personnes accueillies de recevoir, dès l\'arrivée, une formation en Français Langue Étrangère (FLE) et un accompagnement social et professionnel.',
       user_id: admin.user_id, //2
-    },
-    {
-      presentation_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mollis nisi lectus, quis egestas est convallis vitae. Donec maximus lobortis massa, vitae ultrices diam aliquet et. Phasellus at porttitor risus, eget elementum dui. Vestibulum molestie congue pretium. Suspendisse consequat nisl at dapibus viverra. Vestibulum est sapien, rhoncus et diam et, porta faucibus risus. Fusce efficitur lectus. ',
-      user_id: admin.user_id, //3
     }
   ];
   await prisma.presentation.createMany({
@@ -127,7 +122,6 @@ async function main() {
       file_id: 13,
       user_id: admin.user_id,
     },
-    ,
     {
       name: 'Région Hauts-de-France',
       created_at: now,
